f437a5610b8760a54555c1394d77b8c8707fc016
tigerdong <tiger.dong206@hotmail.com>
2016-11-28 11:36:49 -0500

    Create Entity.java

---
+++ Entity.java
@@ -0,0 +1,94 @@
+// Authors: Tiger Dong, Cathy Hua
+// Date: when we finish
+// Description: Cathy you can do this
+
+import java.util.Random;
+
+public class Entity {
+	// Variable declaration
+	private String name;
+	private int maxHealth, currentHealth;
+	private int maxMana, currentMana;
+	private int damageMax, damageMin;
+	private random = new Random();
+
+	// Default constructor
+	Entity() {
+		name = "Untitled";
+		maxHealth = 1;
+		maxMana = 0;
+		damageMax = 0;
+		damageMin = 0;
+	}
+	
+	// Also constructor
+	Entity(String name, int maxHealth, int maxMana, int damageMax, int damageMin) {
+		this.name = name;
+		this.maxHealth = maxHealth;
+		this.maxMana = maxMana;
+		this.damageMax = damageMax;
+		this.damageMin = damageMin;
+	}
+	
+	//
+	public boolean inflict(int damageTaken) {
+		currentHealth -= damageTaken;
+		
+		if (currentHealth > maxHealth)
+			currentHealth = maxHealth;
+		else if (currentHealth < 0)
+			currentHealth = 0;
+		
+		return (currentHealth == 0) ? (false) : (true);
+	}
+	
+	//
+	public boolean heal(int amountHealed) {
+		inflict(-amountHealed);
+	}
+	
+	//
+	public boolean useMana(int manaUsed) {
+		currentMana -= manaUsed;
+		
+		if (currenMana > maxMana)
+			currentMana = maxMana;
+		else if (currentMana < 0)
+			currentMana = 0;
+		
+		return (currentMana == 0) ? (false) : (true);
+	}
+	
+	//
+	public int attack() {
+		return (damageMin+random.nextInt(damageMax-damageMin));
+	}
+	
+	//
+	public String getName() {
+		return name;
+	}
+	
+	//
+	public int getMaxHealth() {}
+	
+	//
+	public int getMaxMana() {}
+	
+	//
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	//
+	public void setMaxHealth(int maxHealth) {}
+	
+	//
+	public void setMaxMana(int maxMana) {}
+	
+	//
+	public void setDamageMax(int damageMax) {}
+	
+	//
+	public void setDamageMin(int damageMin) {}
+}
