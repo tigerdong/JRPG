3619e39960b2426ce63170b25cf4e6b77e181d7b
tigerdong <tiger.dong206@hotmail.com>
2016-11-28 13:49:33 -0500

    Create SquarePG.java

---
+++ SquarePG.java
@@ -0,0 +1,14 @@
+// Authors: Tiger Dong, Cathy Hua
+// Date: yes please
+// Description: 
+
+import java.swing.JFrame;
+
+public class SquarePG extends JFrame {
+	public static void main(String args[]) {
+		GameWindow squarePG = new GameWindow();
+		squarePG.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		squarePG.setSize( 1000, 750 );
+		squarePG.setVisible( true );
+	}
+}
