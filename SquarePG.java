96aae55b1e238d7d20913d355016ac8738412826
CathyWrHua <cathy.hua@gmail.com>
2016-12-20 10:44:11 -0500

    Update SquarePG.java

--- SquarePG.java
+++ SquarePG.java
@@ -2,17 +2,62 @@
 // Date: yes please
 // Description: 
 
-import java.swing.JFrame;
+import java.awt.*;
+import javax.swing.JFrame;
 
 public class SquarePG extends JFrame {
-	public SquarePG() {
+	boolean isRunning = true;
+	int fps = 30;
+	
+	public static void main(String args[]) {
+		SquarePG game = new SquarePG();
+		game.run();
+		System.exit(0);
+	}
+	
+	//Constructor???
+	/*public SquarePG() {
+		
+	}*/
+	
+	//runs game loop
+	public void run(){
+		
+		initialize();
+		
+		while(isRunning){
+			long time = System.currentTimeMillis();
+			
+			update();
+			draw();
+			
+			//delay for each frame
+			time = (1000/fps) - (System.currentTimeMillis()-time);
+			
+			if (time > 0){
+				try{
+					Thread.sleep(time);
+				}
+				catch (Exception e){}
+			}
+			setVisible(false);
+		}
+	}
+	
+	//initial setup
+	void initialize(){
+		setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		setSize( 1000, 750 );
+		setVisible( true );
+	}
+	
+	//input check, movement
+	void update(){
 		
 	}
 	
-	public static void main(String args[]) {
-		GameFrame squarePG = new GameFrame();
-		squarePG.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-		squarePG.setSize( 1000, 750 );
-		squarePG.setVisible( true );
+	//Draws everything
+	void draw(){
+			
 	}
 }
