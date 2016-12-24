0c7633c936f963a7615af67022ae9bb27372ec28
CathyWrHua <cathy.hua@gmail.com>
2016-12-23 23:50:29 -0500

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -2,12 +2,27 @@ package squarepg;
 
 
 import java.awt.Graphics;
-import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.Container;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.event.*;
+
 
 public class GamePanel extends JPanel {
     private Entity Hero;
     private gameState state;
-    
+    private JPanel bottompanel;
+    private JPanel toppanel;
+    private JPanel charcreation;
+    private JButton NButton;
+    private JButton WButton;    
+    private JButton EButton;
+    private JButton SButton;
+    private JButton CButton;
+     
     public enum gameState {
         CHARACTERSELECTION,
         COMBAT,
@@ -15,9 +30,37 @@ public class GamePanel extends JPanel {
         WORLDMAP;
     } 
     
+    public void addComponentsToPane(Container panel, String North, String West, String Centre, String East, String South){
+        NButton = new JButton (North);
+        NButton.setPreferredSize (new Dimension(1000, 100));
+        panel.add(NButton, BorderLayout.PAGE_START);
+        
+        CButton = new JButton(Centre);
+        CButton.setPreferredSize(new Dimension(200, 100));
+        panel.add(CButton, BorderLayout.CENTER);
+
+        WButton = new JButton(West);
+        WButton.setPreferredSize(new Dimension(200, 100));
+        panel.add(WButton, BorderLayout.LINE_START);
+
+        WButton = new JButton(South);
+        WButton.setPreferredSize (new Dimension(1000, 100));
+        panel.add(WButton, BorderLayout.PAGE_END);
+
+        EButton = new JButton(East);
+        EButton.setPreferredSize(new Dimension(200, 100));
+        panel.add(EButton, BorderLayout.LINE_END);
+        
+    }
     
     public GamePanel(){
         Hero = new Entity ();
+        charcreation = new JPanel ();
+        
+        
+        toppanel = new JPanel ();
+        bottompanel = new JPanel (new BorderLayout());
+        addComponentsToPane (bottompanel, "Status", "Items", "Fight", "Flight", "Defend");
     }
     
     public void setState(gameState state){
@@ -28,15 +71,15 @@ public class GamePanel extends JPanel {
     public void update(){
         switch (state){
             case CHARACTERSELECTION:
-                JPanel CharSelection = new JPanel();
+                
                 
                 break;
             case COMBAT:
-                
+                addComponentsToPane (bottompanel, "Status", "Items", "Fight", "Flight", "Defend");
                 
                 
                 break;
-            case SHOP:
+            case SHOP:                
                 break;
             case WORLDMAP:
                 break;
@@ -61,5 +104,26 @@ public class GamePanel extends JPanel {
         }
     
     }
+    
+    private class CombatAction implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            if (event.getSource() == NButton) {
+                
+            }
+            if (event.getSource() == WButton) {
+                
+            }
+            if (event.getSource() == EButton) {
+                
+            }
+            if (event.getSource() == CButton){
+            
+            }
+            if (event.getSource() == SButton){
+            
+            }
+        }
+    }
+    
     
 }
