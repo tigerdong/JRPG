b670baf10ae86b6f2105660a1f0ac864b00949dd
Tiger (Xiao Min) Dong <tiger.dong206@hotmail.com>
2016-12-24 23:32:38 -0500

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -1,218 +1,188 @@
-package squarepg;
-
+// Authors: Tiger Dong, Cathy Hua
+// Date: yes please
+// Description: 
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.*;
 import java.awt.event.*;
+import javax.swing.*;
 
+public class SquarePG extends JFrame {
+    private JPanel menuPanel = new JPanel();
+    private JButton playButton = new JButton("Play");
+    private JButton optionsButton = new JButton("Options");
+    private JButton aboutButton = new JButton("About");
+    private JButton back1Button = new JButton("Back");
+    private JButton back2Button = new JButton("Back");
+    
+    //private GamePanel gamePanel;
+    //private InteractionsPanel interactionsPanel;
+    
+    private JPanel optionsPanel = new JPanel();
+    private JLabel replaceMe = new JLabel("OPTIONS WILL GO HERE ;-)");
+    
+    private JPanel aboutPanel = new JPanel();
+    private JLabel aboutLabel = new JLabel("<html><center>ABOUT<br><br>Authors:<br>Tiger Dong, Cathy Hua<br><br>Last Revised:<br>DATE HERE<br><br>Compiler:<br>JDK 1.8.0_101<br><br>Version:<br>Java SE 8</html>");
+    
+    private JPanel selectionPanel = new JPanel();
+    private JPanel characterPanel = new JPanel();
+    private JPanel namePanel = new JPanel();
+    private JLabel selectionLabel = new JLabel("Yeah, why not?");
+    private JLabel nameLabel = new JLabel("Square's Name:");
+    private JTextField nameTextField = new JTextField(15);
+    private JToggleButton redButton = new JToggleButton("Red");
+    private JToggleButton yellowButton = new JToggleButton("Yellow");
+    private JToggleButton blueButton = new JToggleButton("Blue");
+    private JButton startButton = new JButton("Start!");
+    
+    private boolean isRunning = false;
+    private int FPS = 30;
+    private int playerClass = 0;
+    private String playerName = "";
 
-public class GamePanel extends JPanel {
-    private Entity Hero;
-    private Entity Enemy;
-    private gameState state = gameState.CHARACTERSELECTION;
-    
-    private JPanel gameScreen;
-    private JPanel bottompanel;
-    private JPanel toppanel;
-    
-    private JButton NButton;
-    private JButton WButton;    
-    private JButton EButton;
-    private JButton SButton;
-    private JButton CButton;
-
-    private JLabel nameChoose = new JLabel("Enter Character Name: ");    
-    private JTextField name = new JTextField (20);
-    
-    private JLabel classChoice = new JLabel ("Please choose a class: "); 
-    private JRadioButton warrior = new JRadioButton ("Warrior");
-    private JRadioButton archer = new JRadioButton ("Archer");
-    private JRadioButton mage = new JRadioButton ("Mage");
-    
-    
-    public enum gameState {
-        CHARACTERSELECTION,
-        COMBAT,
-        SHOP,
-        WORLDMAP;
-    } 
-    
-    //Creates N/E/S/W panels of size W: 1000, H: 300 pixels
-    public void addComponentsToPane(Container panel, String North, String West, String Centre, String East, String South){
-        NButton = new JButton (North);
-        NButton.setPreferredSize (new Dimension(1000, 100));
-        panel.add(NButton, BorderLayout.PAGE_START);
-        
-        
-        CButton = new JButton(Centre);
-        CButton.setPreferredSize(new Dimension(333, 100));
-        panel.add(CButton, BorderLayout.CENTER);
-
-        WButton = new JButton(West);
-        WButton.setPreferredSize(new Dimension(300, 100));
-        panel.add(WButton, BorderLayout.LINE_START);
-
-        WButton = new JButton(South);
-        WButton.setPreferredSize (new Dimension(1000, 100));
-        panel.add(WButton, BorderLayout.PAGE_END);
-
-        EButton = new JButton(East);
-        EButton.setPreferredSize(new Dimension(333, 100));
-        panel.add(EButton, BorderLayout.LINE_END);
+    public static void main(String args[]) {
+        SquarePG game = new SquarePG();
+        game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        game.setSize(1000, 750);
+        game.setVisible(true);
     }
     
-    
-    public boolean createCharacter (String name, int choosenClass ){
-        Hero.setName(name);
-
-        switch(choosenClass){
-            case 0: // Warrior class
-                Hero.setMaxHealth(300);
-                Hero.setMaxMana(50);
-                Hero.setDamageMax(50);
-                Hero.setDamageMin(40);
-                break;
-                
-            case 1: // Ranger Class
-                Hero.setMaxHealth(200);
-                Hero.setMaxMana(100);
-                Hero.setDamageMax(70);
-                Hero.setDamageMin(60);
-                break;
-                
-            case 2: //Mage Class
-                Hero.setMaxHealth(100);
-                Hero.setMaxMana(300);
-                Hero.setDamageMax(60);
-                Hero.setDamageMin(50);
-                break;
-                
-            default: //Error
-                return false;
-        }
+    // Constructor
+    public SquarePG() {
+        super("SquarePG");
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.PAGE_AXIS));
         
-        Hero.setPosX(0);
-        Hero.setPosY(0);
+        playButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        optionsButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        aboutButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        replaceMe.setAlignmentX(Component.CENTER_ALIGNMENT);
+        aboutLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        back1Button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        back2Button.setAlignmentX(Component.CENTER_ALIGNMENT);
         
-        return true;
+        menuPanel.setLayout(new BoxLayout(menuPanel, BoxLayout.PAGE_AXIS));
+        menuPanel.add(playButton);
+        menuPanel.add(optionsButton);
+        menuPanel.add(aboutButton);
+        
+        optionsPanel.setLayout(new BoxLayout(optionsPanel, BoxLayout.PAGE_AXIS));
+        optionsPanel.add(replaceMe);
+        optionsPanel.add(back1Button);
+        
+        aboutPanel.setLayout(new BoxLayout(aboutPanel, BoxLayout.PAGE_AXIS));
+        aboutPanel.add(aboutLabel);
+        aboutPanel.add(back2Button);
+        
+        selectionPanel.setLayout(new BoxLayout(selectionPanel, BoxLayout.PAGE_AXIS));
+        characterPanel.setLayout(new BoxLayout(characterPanel, BoxLayout.LINE_AXIS));
+        namePanel.setLayout(new BoxLayout(namePanel, BoxLayout.LINE_AXIS));
+        characterPanel.add(redButton);
+        characterPanel.add(yellowButton);
+        characterPanel.add(blueButton);
+        namePanel.add(nameLabel);
+        namePanel.add(nameTextField);
+        selectionPanel.add(selectionLabel);
+        selectionPanel.add(characterPanel);
+        selectionPanel.add(namePanel);
+        selectionPanel.add(startButton);
+        startButton.setEnabled(false);
+        nameTextField.setEditable(false);
+        nameTextField.setMaximumSize(nameTextField.getPreferredSize());
+        
+        contentPane.add(menuPanel);
+        
+        ActionHandler actionHandler = new ActionHandler();
+        playButton.addActionListener(actionHandler);
+        optionsButton.addActionListener(actionHandler);
+        aboutButton.addActionListener(actionHandler);
+        back1Button.addActionListener(actionHandler);
+        back2Button.addActionListener(actionHandler);
+        redButton.addActionListener(actionHandler);
+        yellowButton.addActionListener(actionHandler);
+        blueButton.addActionListener(actionHandler);
+        startButton.addActionListener(actionHandler);
+        nameTextField.addActionListener(actionHandler);
     }
     
-    public GamePanel(){
-        Hero = new Entity ();
-        gameScreen = new JPanel (new GridLayout(0, 1));
-        toppanel = new JPanel ();
-        bottompanel = new JPanel (new BorderLayout());
+    // Runs game loop
+    private void runGameLoop() {
+        Thread loop = new Thread() {
+            public void run() {
+                gameLoop();
+            }
+        };
+        loop.start();
     }
     
-    public void setState(gameState state){
-        this.state = state;
-    }
-    
-    public void init() {
-        switch (state){
-            case CHARACTERSELECTION:
-                
-                ButtonGroup group = new ButtonGroup();
-                group.add(warrior);
-                group.add(archer);
-                group.add(mage);
-                
-                NButton = new JButton ("Enter");
-                NButton.setSize(new Dimension(100, 100));
-                
-                gameScreen.add(nameChoose);
-                gameScreen.add(name);
-                gameScreen.add(classChoice);
-                gameScreen.add(warrior);
-                gameScreen.add(archer);
-                gameScreen.add(mage);
-                gameScreen.add(NButton);
-                
-                
-                break;
-            case COMBAT:
-                addComponentsToPane (bottompanel, "Status", "Items", "Fight", "Flight", "Defend");
-                //draw the initial top panel
-                gameScreen.add(toppanel);
-                gameScreen.add(bottompanel);
-                
-                //Sample enemy for test purposes
-                Enemy = new Entity("Enemy", 50, 20, 20, 15, 10, 100);
-                
-                CombatAction combatAction = new CombatAction();
-                NButton.addActionListener(combatAction);
-                EButton.addActionListener(combatAction);
-                CButton.addActionListener(combatAction);
-                WButton.addActionListener(combatAction);
-                SButton.addActionListener(combatAction);
-
-                
-                
-                break;
-            case SHOP:
-                break;
-            case WORLDMAP:
-                break;
+    // Actual game loop
+    private void gameLoop() {
+        while(isRunning){
+            long time = System.currentTimeMillis();
+            //gamePanel.update();
+            //gamePanel.draw();
+            // Delay for each frame
+            time = (1000/FPS) - (System.currentTimeMillis()-time);
+            
+            if (time > 0){
+                try{
+                    Thread.sleep(time);
+                }
+                catch (Exception e){}
+            }
         }
     }
     
-    
-    public void update(){
-        switch (state){
-            case CHARACTERSELECTION:
-                
-                
-                break;
-            case COMBAT:
-                
-                break;
-            case SHOP:                
-                break;
-            case WORLDMAP:
-                break;
-        
-        }
-    
-    }
-    
-    public void draw(){
-        switch(state){
-            case CHARACTERSELECTION:
-                
-                break;
-            case COMBAT:
-                break;
-            case SHOP:
-                break;
-            case WORLDMAP:
-                break;
-        
-        
-        }
-    
-    }
-    
-    private class CombatAction implements ActionListener {
+    // Private inner class for ActionListener event handling
+    private class ActionHandler implements ActionListener {
         public void actionPerformed(ActionEvent event) {
-            if (event.getSource() == NButton) {//Status? button
-                
-            }
-            if (event.getSource() == WButton) {// Item button
-                
-            }
-            if (event.getSource() == EButton) {// Run button
-                
-            }
-            if (event.getSource() == CButton){//Fight button
-            
-            }
-            if (event.getSource() == SButton){//Defend button
-            
+            if (event.getSource() == playButton) {
+                isRunning = true;
+                remove(menuPanel);
+                add(selectionPanel);
+                validate();
+                repaint();
+                runGameLoop();
+            } if (event.getSource() == optionsButton) {
+                remove(menuPanel);
+                add(optionsPanel);
+                validate();
+                repaint();
+            } if (event.getSource() == aboutButton) {
+                remove(menuPanel);
+                add(aboutPanel);
+                validate();
+                repaint();
+            } if (event.getSource() == back1Button) {
+                remove(optionsPanel);
+                add(menuPanel);
+                validate();
+                repaint();
+            } if (event.getSource() == back2Button) {
+                remove(aboutPanel);
+                add(menuPanel);
+                validate();
+                repaint();
+            } if (event.getSource() == redButton) {
+                playerClass = 0;
+                nameTextField.setEditable(true);
+                yellowButton.setSelected(false);
+                blueButton.setSelected(false);
+            } if (event.getSource() == yellowButton) {
+                playerClass = 1;
+                nameTextField.setEditable(true);
+                redButton.setSelected(false);
+                blueButton.setSelected(false);
+            } if (event.getSource() == blueButton) {
+                playerClass = 2;
+                nameTextField.setEditable(true);
+                redButton.setSelected(false);
+                yellowButton.setSelected(false);
+            } if (event.getSource() == nameTextField && event.getActionCommand().length() > 0) {
+                playerName = event.getActionCommand();
+                startButton.setEnabled(true);
+            } if (event.getSource() == startButton) {
+                //gamePanel.functionName(playerName, playerClass);
             }
         }
     }
-    
-    
 }
