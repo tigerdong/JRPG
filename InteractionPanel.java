c152210848e2b33642216affb8191467a7e8ac14
CathyWrHua <cathy.hua@gmail.com>
2016-12-27 21:59:46 -0500

    Update InteractionPanel.java

--- InteractionPanel.java
+++ InteractionPanel.java
@@ -12,10 +12,9 @@ public class InteractionPanel extends JPanel{
     private JButton RTButton = new JButton(); //3
     private JButton RBButton = new JButton(); //4
     private gameState state = gameState.COMBAT;
-    private int buttonPressed = 0; //Action communication with Gamepanel, number depends on button number pressed
+    private int buttonPressed; //Action communication with Gamepanel, number depends on button number pressed
+    private int buttonLayer;
     private CombatAction combatAction = new CombatAction();
-    private ShopAction shopAction = new ShopAction();
-    private WorldAction worldAction = new WorldAction();
     
     public enum gameState {
         COMBAT,
@@ -32,6 +31,11 @@ public class InteractionPanel extends JPanel{
         add(LBButton);
         add(RBButton);
         
+        LTButton.addActionListener(combatAction);
+        LBButton.addActionListener(combatAction);
+        RTButton.addActionListener(combatAction);
+        RBButton.addActionListener(combatAction);
+        
         init();
     }
     
@@ -45,6 +49,7 @@ public class InteractionPanel extends JPanel{
     
     public void clearButtonPressed (){
         buttonPressed = 0 ;
+        buttonLayer = 0;
     }
     
     public int getButtonPressed (){
@@ -65,181 +70,97 @@ public class InteractionPanel extends JPanel{
                 LBButton.setText("Abilities");
                 RTButton.setText("Items");
                 RBButton.setText("Flee");
-                
-                LTButton.addActionListener(combatAction);
-                LBButton.addActionListener(combatAction);
-                RTButton.addActionListener(combatAction);
-                RBButton.addActionListener(combatAction);
                 break;
             case SHOP:
                 LTButton.setText("Consumables");
                 LBButton.setText("Sell Items");
                 RTButton.setText("Equipment");
                 RBButton.setText("World Map");
-                
-                LTButton.addActionListener(shopAction);
-                LBButton.addActionListener(shopAction);
-                RTButton.addActionListener(shopAction);
-                RBButton.addActionListener(shopAction);
                 break;
             case WORLDMAP:
-                LTButton.setText("Shop");
+                LTButton.setText("Skill Tree");
                 LBButton.setText("Items");
                 RTButton.setText("Character");
-                RBButton.setText("Skill Tree");
-                
-                LTButton.addActionListener(worldAction);
-                LBButton.addActionListener(worldAction);
-                RTButton.addActionListener(worldAction);
-                RBButton.addActionListener(worldAction);
+                RBButton.setText("Shop");
                 break;
         }
     }
     
     
     public void update(){
-        switch (state){
-            case COMBAT:
-                    LTButton.setText("Use");
-                    RTButton.setText ("Info");
-                    LBButton.setText("Back");
-                    RBButton.setText("");
-                    RBButton.setEnabled(false);
-                
-                break;
-            case SHOP:
-                
-                    switch(buttonPressed){
-                        case 1:
-                            LTButton.setText("case 1");
-                            break;
-                        case 3:
-                            LTButton.setText("case 3");
-                            break;
-                        case 2:
-                            LTButton.setText("case 2");
-                            break;
+        if (buttonLayer == 0){
+            buttonLayer =1;
+            switch (state){
+                case COMBAT:
+                    if (buttonPressed != 4){
+                        LTButton.setText("Use");
+                        RTButton.setText ("Info");
+                        RBButton.setText("Back");
+                        LBButton.setText("");
+                        LBButton.setEnabled(false);
                     }
-                    LBButton.setText("Back");
-                    RTButton.setText("Info");
-                    RBButton.setText("");
-                    RBButton.setEnabled(false);
-                break;
-            case WORLDMAP:
-                
-                break;
-        
-        }
-    }
-    
-    private void removeListener (){
-        switch(state){
-            case COMBAT:
-                LBButton.removeActionListener(combatAction);
-                LTButton.removeActionListener(combatAction);
-                RTButton.removeActionListener(combatAction);
-                RBButton.removeActionListener(combatAction);
+                    else {
+                        state = gameState.WORLDMAP;
+                        init();
+                    }
                 break;
             case SHOP:
-                LBButton.removeActionListener(shopAction);
-                LTButton.removeActionListener(shopAction);
-                RBButton.removeActionListener(shopAction);
-                RTButton.removeActionListener(shopAction);
+                RBButton.setText("Back");
+                RTButton.setText("Info");
+                LBButton.setText("");
+                LBButton.setEnabled(false);
+                switch(buttonPressed){
+                    case 1: case 3:
+                        LTButton.setText("Buy");
+                        break;
+                    case 2:
+                        LTButton.setText("Sell");
+                        break;
+                    case 4:
+                        state = gameState.WORLDMAP;
+                        init();
+                }
                 break;
             case WORLDMAP:
-                LBButton.removeActionListener(worldAction);
-                LTButton.removeActionListener(worldAction);
-                RBButton.removeActionListener(worldAction);
-                RTButton.removeActionListener(worldAction);
+                switch(buttonPressed){
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        state = gameState.SHOP;
+                        init();
+                        break;
+                }
                 break;
+            }        
+        }
+        else if (buttonLayer == 1 && buttonPressed == 4){
+            init();
         }
     }
     
     private class CombatAction implements ActionListener {
         public void actionPerformed(ActionEvent event) {
             if (event.getSource() == LTButton) {//Attack button
-                if (buttonPressed == 0){
-                    buttonPressed = 1;
-                    update();
-                }
+                buttonPressed = 1;
+                update();  
             }
             if (event.getSource() == LBButton) {// Abilities button
-                if (buttonPressed == 0){
-                    buttonPressed = 2;
-                    update();
-                }
-                else {
-                    init();
-                }
+                buttonPressed = 2;
+                update(); 
             }
             if (event.getSource() == RTButton) {// Items button
-                if(buttonPressed == 0){
-                    buttonPressed = 3;
-                    update();
-                }
+                buttonPressed = 3;
+                update(); 
             }
             if (event.getSource() == RBButton){//Flee button
                 buttonPressed = 4;
-                removeListener();
-                state = gameState.WORLDMAP;
-                init();
+                update();
             }
         }
     }
-    
-    private class ShopAction implements ActionListener {
-        public void actionPerformed (ActionEvent event){
-        if (event.getSource() == LTButton) {//Buy Consumables button
-                if (buttonPressed == 0){
-                    buttonPressed = 1;
-                    update();
-                }
-            }
-            if (event.getSource() == LBButton) {//Sell button
-                if (buttonPressed == 0){
-                    buttonPressed = 2;
-                    update();
-                }
-                else {
-                    init();
-                }
-            }
-            if (event.getSource() == RTButton) {//Equipment buy button
-                if (buttonPressed == 0){
-                    buttonPressed = 3;
-                    update();
-                }
-            }
-            if (event.getSource() == RBButton){//Return to WorldMap button
-                buttonPressed = 4;
-                removeListener();
-                state = gameState.WORLDMAP;
-                init();
-            }
-        }
-    }
-    
-    private class WorldAction implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            if (event.getSource() == LTButton) {//Shop button
-                buttonPressed = 1;
-                removeListener();
-                state = gameState.SHOP;
-                init();
-            }
-            if (event.getSource() == LBButton) {//Items button
-                buttonPressed = 2;
-                update();
-            }
-            if (event.getSource() == RTButton) {//Characters button
-                buttonPressed = 3;
-                update();
-            }
-            if (event.getSource() == RBButton){//Skill Tree button
-                buttonPressed = 4;
-                update();
-            }
-        }
-    } 
 }
 
