e43ce8dbe320556f722646b521087cb29a1fead0
CathyWrHua <cathy.hua@gmail.com>
2016-12-29 10:54:01 -0500

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -4,27 +4,31 @@
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.concurrent.ThreadLocalRandom;
 import javax.swing.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements KeyListener{
     private Entity hero;
     private Entity enemy;
-    private GameState state = GameState.COMBAT;
+    private GameState state = GameState.WORLDMAP;
     
     private boolean isEnemyTurn = false;
     private final int RED = 0;
     private final int YELLOW = 1;
     private final int BLUE = 2;
     
-    public enum GameState {
-        COMBAT,
-        SHOP,
-        WORLDMAP;
-    } 
+    private Image test;
     
     public GamePanel(){
         hero = new Entity();
         setBackground(Color.WHITE);
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+    }
+    
+    public GameState getState(){
+        return state;
     }
     
     public boolean createCharacter (String name, int chosenClass){
@@ -59,8 +63,8 @@ public class GamePanel extends JPanel {
                 return false;
         }
         
-        hero.setPosX(0);
-        hero.setPosY(0);
+        hero.setPosX(100);
+        hero.setPosY(100);
         init();
         
         return true;
@@ -77,12 +81,25 @@ public class GamePanel extends JPanel {
             case SHOP:
                 break;
             case WORLDMAP:
+                this.requestFocus(true);
                 break;
         }
     }
     
     public void update(int action) {
-        switch (state) {
+        if (action == 4){
+            switch (state){
+                case COMBAT: case SHOP:
+                    state = GameState.WORLDMAP;
+                    break;
+                case WORLDMAP:
+                    state = GameState.SHOP;
+                    break;
+            }
+            init();        
+        }
+        else {
+            switch (state) {
             case COMBAT:
                 switch (action) {
                 case 1:
@@ -113,14 +130,94 @@ public class GamePanel extends JPanel {
                 }
                 hero.update();
                 enemy.update();
+                if (enemy.getCurrentHealth() == 0){
+                    state = GameState.WORLDMAP;
+                    init();
+                }
+                if (hero.getCurrentHealth() == 0){
+                    System.out.println(hero.getName() + " is DEAD!");
+                    System.exit(0);
+                }
                 break;
-            case SHOP:                
+            case SHOP:
+                switch (action){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                }
                 break;
             case WORLDMAP:
+                switch (action){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                }
                 break;
+            }
         }
     }
     
+    public void up (){
+        hero.setPosY(hero.getPosY() - 50);
+        if (stepTrigger()){
+            state = GameState.COMBAT;
+            init();
+        }
+    }
+    
+    public void down(){
+        hero.setPosY(hero.getPosY() + 50);
+        if (stepTrigger()){
+            state = GameState.COMBAT;
+            init();
+        }
+    }
+    public void left(){
+        hero.setPosX(hero.getPosX() - 50);
+        if (stepTrigger()){
+            state = GameState.COMBAT;
+            init();
+        }
+    }
+    public void right (){
+        hero.setPosX(hero.getPosX() + 50);
+        if (stepTrigger()){
+            state = GameState.COMBAT;
+            init();
+        }
+    }
+    
+    public void keyPressed (KeyEvent e){
+        if (state == GameState.WORLDMAP){
+            int code = e.getKeyCode();
+            if (code == KeyEvent.VK_UP || code == KeyEvent.VK_W){
+                up();
+            }
+            if (code == KeyEvent.VK_DOWN || code == KeyEvent.VK_S){
+                down();
+            }
+            if (code == KeyEvent.VK_LEFT || code == KeyEvent.VK_A){
+                left();
+            }
+            if (code == KeyEvent.VK_RIGHT || code == KeyEvent.VK_D){
+                right();
+            }
+        }
+    }
+    
+    public void keyTyped(KeyEvent e){}
+    public void keyReleased(KeyEvent e){}
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         switch (state) {
@@ -131,7 +228,21 @@ public class GamePanel extends JPanel {
             case SHOP:
                 break;
             case WORLDMAP:
+                ImageIcon thing = new ImageIcon(this.getClass().getResource("Test.png"));
+                test = thing.getImage();
+        
+                Graphics2D g2d = (Graphics2D)g;
+                g2d.drawImage(test, hero.getPosX(), hero.getPosY(), this);
                 break;
         }
+    }
+    
+    private boolean stepTrigger (){
+        int randomNum = ThreadLocalRandom.current().nextInt(0, 101);
+        
+        if (randomNum < 20){
+            return true;
+        }
+        return false;
     }
 }
