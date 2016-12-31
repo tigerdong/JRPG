2b9da5a4e4e10bdfe78e7f09b99123429db2d034
CathyWrHua <cathy.hua@gmail.com>
2016-12-31 14:31:55 -0500

    Update GameState.java

--- GameState.java
+++ GameState.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package squarepg;
+package SquarePG;
 
-
-    public enum GameState{
-        COMBAT, 
-        SHOP, 
-        WORLDMAP;
-    }
+public enum GameState{
+    COMBAT, 
+    SHOP, 
+    WORLDMAP;
+}
