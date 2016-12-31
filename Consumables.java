287d809ab11efda8dffa92d588f0cc1da0a29bea
CathyWrHua <cathy.hua@gmail.com>
2016-12-30 23:13:47 -0500

    Update Consumables.java

--- Consumables.java
+++ Consumables.java
@@ -1,27 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package squarepg;
 
-/**
- *
- * @author Cathy
- */
 import java.util.ArrayList;
 
 //List of the full set of possible consumables available in game;
 public class Consumables {
     private ArrayList<Item> list = new ArrayList<Item>();
+    private Item potion = new Item (10, 0, "Potion", "Restores a little bit of health", 100, 50);
+    private Item elixir = new Item (0, 10, "Elixir", "Restores a little bit of mana", 100, 50);
+    private Item Mipotion = new Item (100, 0, "Mid Potion", "Restores a fair amount of health", 200, 100);
+    private Item Mielixir = new Item (0, 100, "Mid Elixir", "Restores a fair amount of mana", 200, 100);
+    private Item Restore = new Item (200, 200, "Mid Restore", "Restores health and mana", 200, 100);
     
     Consumables(){
-        Item potion = new Item (10, 0, "Potion", "Restores a little bit of health");
-        Item elixir = new Item (0, 10, "Elixir", "Restores a little bit of mana");
-        Item Mipotion = new Item (100, 0, "Mid Potion", "Restores a fair amount of health");
-        Item Mielixir = new Item (0, 100, "Mid Elixir", "Restores a fair amount of mana");
-        Item Restore = new Item (200, 200, "Mid Restore", "Restores health and mana");
-        
         list.add(potion);
         list.add(elixir);
         list.add(Mipotion);
