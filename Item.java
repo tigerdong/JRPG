57d03722d4269e87c67b93b6d79d29a04026f1c6
CathyWrHua <cathy.hua@gmail.com>
2016-12-30 23:15:17 -0500

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,19 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package squarepg;
 
-/**
- *
- * @author Cathy
- */
 public class Item {
     private int gainHealth, gainMP;
-    private int ATK, MAG, DEF;
+    private int ATK, MAG, DEF; // ATK raises maxdamage, MAG raises maxdamage and mana, DEF raises maxhealth
     private String name;
     private String description;
+    private int buyPrice, sellPrice;
+    private int stock = 0;
     
     Item(){
         gainHealth = 0;
@@ -23,21 +16,74 @@ public class Item {
     }
     
     //for consumables
-    Item (int gainHealth, int gainMP, String name, String description){
+    Item (int gainHealth, int gainMP, String name, String description, int buyPrice, int sellPrice){
         this.gainHealth = gainHealth;
         this.gainMP = gainMP;
         this.name = name;
         this.description = description;
         ATK = MAG = DEF = 0;
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
     }
     
     //For Weapons and Armor
-    Item (int ATK, int MAG, int DEF, String name, String description){
+    Item (int ATK, int MAG, int DEF, String name, String description, int buyPrice, int sellPrice){
         this.ATK = ATK;
         this.MAG = MAG;
         this.DEF = DEF;
         this.name = name;
         this.description = description;
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
     }
     
+    public int getStock (){
+        return stock;
+    }
+    
+    public boolean changeStock (int change){
+        if (stock + change >= 0){
+            stock += change;
+        }
+        else {
+            return false;
+        }
+        return true;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getDescription(){
+        return description;
+    }
+    
+    public int getBuyPrice (){
+        return buyPrice;
+    }
+    
+    public int getSellPrice(){
+        return sellPrice;
+    }
+    
+    public int getGainHealth(){
+        return gainHealth;
+    }
+    
+    public int getGainMP(){
+        return gainMP;
+    }
+    
+    public int getATK (){
+        return ATK;
+    }
+    
+    public int getDEF (){
+        return DEF;
+    }
+    
+    public int getMAG () {
+        return MAG;
+    }
 }
