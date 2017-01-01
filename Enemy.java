214f9afc8e2556bd2ccf9d38f730f30ebab6fdd5
Tiger (Xiao Min) Dong <tiger.dong206@hotmail.com>
2016-12-31 21:44:00 -0500

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -9,15 +9,21 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Enemy extends Entity {
+    private ImageIcon enemyAvatar;
+    
     Enemy() {
         super();
+        enemyAvatar = new ImageIcon(this.getClass().getResource("enemy.png"));
     }
     
     Enemy(String name, int maxHealth, int maxMana, int damageMax, int damageMin, int gold) {
         super(name, maxHealth, maxMana, damageMax, damageMin, gold);
+        enemyAvatar = new ImageIcon(this.getClass().getResource("enemy.png"));
     }
     
     public void draw(Graphics g) {
         Graphics2D g2d = (Graphics2D)g;
+        
+        g2d.drawImage(enemyAvatar.getImage(), 100, 250, null);
     }
 }
